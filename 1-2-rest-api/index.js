d99bf4f9423536cc0479776dfb6e7bbef78609fb
이건선 <igeonseon@igeonseon-ui-Macmini.local>
2024-08-08 17:20:31 +0900

    fix

--- 1-2-rest-api/index.js
+++ 1-2-rest-api/index.js
@@ -44,14 +44,14 @@ app.use('/api/supply', supplyRouter)
 const filePath = path.join(__dirname, '.well-known', 'apple-app-site-association');
 
 // '/scan' 경로에 대한 GET 요청 처리
-app.get('/scan', (req, res) => {
+app.get('/.well-known/apple-app-site-association', (req, res) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             // 파일 읽기 오류 처리
             res.status(500).send('Unable to read the file');
         } else {
             // 성공적으로 파일 내용을 읽었을 때 JSON 형식으로 클라이언트에 전송
-            res.header('Content-Type', 'application/json').send(data);
+            res.setHeader('Content-Type', 'application/json').send(data);
         }
     });
 });
