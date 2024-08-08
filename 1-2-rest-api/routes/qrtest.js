60a85b5734d61afcb61f43e136ec20614ed9d7d4
이건선 <igeonseon@igeonseon-ui-Macmini.local>
2024-08-08 16:46:27 +0900

    fix

--- 1-2-rest-api/routes/qrtest.js
+++ 1-2-rest-api/routes/qrtest.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const router = express.Router();
-const axios = require('axios');
+// const express = require('express');
+// const router = express.Router();
+// const axios = require('axios');
 
-const targetUrl = 'http://www.tqrtqr.shop/.well-known/apple-app-site-association';
+// const targetUrl = 'http://www.tqrtqr.shop/.well-known/apple-app-site-association';
 
-router.get('/', async (req, res) => {
-  try {
-    const response = await axios.get(targetUrl, {
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-    res.json(response.data);
-  } catch (error) {
-    console.error('Error fetching the URL:', error);
-    res.status(500).send('Error fetching the URL');
-  }
-});
+// router.get('/', async (req, res) => {
+//   try {
+//     const response = await axios.get(targetUrl, {
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     });
+//     res.json(response.data);
+//   } catch (error) {
+//     console.error('Error fetching the URL:', error);
+//     res.status(500).send('Error fetching the URL');
+//   }
+// });
 
-module.exports = router;
+// module.exports = router;
