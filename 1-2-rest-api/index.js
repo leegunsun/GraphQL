3728ab7417741a23aa6488846f659e7fbc8846ce
이건선 <igeonseon@igeonseon-ui-Macmini.local>
2024-08-08 17:47:29 +0900

    fix

--- 1-2-rest-api/index.js
+++ 1-2-rest-api/index.js
@@ -14,7 +14,7 @@ const health = require('./routes/health.js')
 const port = 3000
 
 app.use(express.json())
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
 
 app.use('/health', health)
 app.use('/api/team', teamRouter)
@@ -24,23 +24,23 @@ app.use('/api/software', softwareRouter)
 app.use('/api/equipment', equipmentRouter)
 app.use('/api/supply', supplyRouter)
 
-// 파일 경로 설정1
+
 const filePath = path.join(__dirname, '.well-known', 'apple-app-site-association');
 
-// '/.well-known/apple-app-site-association' 경로에 대한 GET 요청 처리
 app.get('/.well-known/apple-app-site-association', (req, res) => {
     fs.readFile(filePath, 'utf8', (err, data) => {
         if (err) {
             // 파일 읽기 오류 처리
             res.status(500).send('Unable to read the file');
         } else {
-            // 성공적으로 파일 내용을 읽었을 때 JSON 형식으로 클라이언트에 전송
+            // 캐싱 방지 헤더 설정
+            res.setHeader('Cache-Control', 'no-store');
             res.setHeader('Content-Type', 'application/json');
             res.status(200).send(data);
-            // res.send(data);
         }
     });
 });
+
 
 app.listen(port, () => {
   console.log(`REST API listening at http://localhost:${port}`)
