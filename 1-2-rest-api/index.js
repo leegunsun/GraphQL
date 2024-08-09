857da0113ae603f7841e2fd898605e292837cde9
이건선 <igeonseon@igeonseon-ui-Macmini.local>
2024-08-09 09:25:04 +0900

    fix

--- 1-2-rest-api/index.js
+++ 1-2-rest-api/index.js
@@ -24,17 +24,34 @@ app.use('/api/software', softwareRouter)
 app.use('/api/equipment', equipmentRouter)
 app.use('/api/supply', supplyRouter)
 
-// 정적 파일을 위한 경로 설정
-const wellKnownDirectory = path.join(__dirname, '.well-known');
+const filePath = path.join(__dirname, '.well-known', 'apple-app-site-association');
 
-// .well-known 폴더를 정적으로 제공하되, 특정 파일에 대해 MIME 타입을 설정
-app.use('/.well-known', express.static(wellKnownDirectory, {
-    setHeaders: (res, path) => {
-        if (path.endsWith('apple-app-site-association')) {
+app.get('/.well-known/apple-app-site-association', (req, res) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+            // 파일 읽기 오류 처리
+            res.status(500).send('Unable to read the file');
+        } else {
+            // 캐싱 방지 헤더 설정
+            res.setHeader('Cache-Control', 'no-store');
             res.setHeader('Content-Type', 'application/json');
+            res.status(200).send(data);
         }
-    }
-}));
+    });
+});
+
+// // 정적 파일을 위한 경로 설정
+// const wellKnownDirectory = path.join(__dirname, '.well-known');
+
+// // .well-known 폴더를 정적으로 제공하되, 특정 파일에 대해 MIME 타입을 설정
+// app.use('/.well-known', express.static(wellKnownDirectory, {
+//     setHeaders: (res, path) => {
+//         if (path.endsWith('apple-app-site-association')) {
+//             res.setHeader('Content-Type', 'application/json');
+//         }
+//     }
+// }));
+
 
 
 app.listen(port, () => {
